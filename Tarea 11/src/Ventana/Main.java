62e21a13b7fb2abf1a1bb54d097788c28a5b37bb
ALEXIS <gustavoalexislopezrivas@gmail.com>
2026-04-28 00:29:34 -0600

    Primer commit

---
+++ Tarea 11/src/Ventana/Main.java
@@ -0,0 +1,13 @@
+package Ventana;
+import Ventana.VentanaImc;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Main {
+    static void main() {
+
+        VentanaImc ventanaImc = new VentanaImc("Descubra su IMC",12000,600);
+
+
+
+    }
+}
