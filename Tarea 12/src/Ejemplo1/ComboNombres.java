838c4dae2ba5ce68db151ea50e8eebb514b65088
ALEXIS <gustavoalexislopezrivas@gmail.com>
2026-05-03 15:19:42 -0600

    Primer commit - proyecto Swing

---
+++ Tarea 12/src/Ejemplo1/ComboNombres.java
@@ -0,0 +1,51 @@
+package Ejemplo1;
+import javax.swing.*;
+import javax.swing.event.ListDataListener;
+import java.util.ArrayList;
+public class ComboNombres implements ComboBoxModel {
+    private ArrayList<String> nombres;
+    private String selectedItem;
+
+    public ComboNombres(ArrayList<String> nombres) {
+        this.nombres = nombres;
+    }
+
+    public ArrayList<String> getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(ArrayList<String> nombres) {
+        this.nombres = nombres;
+    }
+
+    @Override
+    public void setSelectedItem(Object anItem) {
+        selectedItem = (String) anItem;
+
+    }
+
+    @Override
+    public Object getSelectedItem() {
+        return selectedItem;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        return null;
+    }
+
+    @Override
+    public void addListDataListener(ListDataListener l) {
+
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener l) {
+
+    }
+}
---
+++ Tarea 12/src/Ejemplo1/Main.java
@@ -0,0 +1,13 @@
+package Ejemplo1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Puedes elegir cuál ventana abrir
+
+        // Ventana 1
+        new VentanaUno();
+
+        // Si quieres abrir también la segunda, descomenta:
+        // new VentanaDos();
+    }
+}
